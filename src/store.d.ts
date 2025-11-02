80e61196f80684b9ad6d9e7d1a01b4b4da6881f4
Payfaucet <ryzefn247@gmail.com>
2025-11-02 20:45:00 +0400

    Delete src/store.d.ts

--- src/store.d.ts
+++
@@ -1,7 +0,0 @@
-interface AppState {
-    count: number;
-    increment: () => void;
-    decrement: () => void;
-}
-export declare const useAppStore: import("zustand").UseBoundStore<import("zustand").StoreApi<AppState>>;
-export {};
